7652e3bbd35becae07109c085bf97aa30e4e2c7f
Harris Levine <acyclicgraphs@gmail.com>
2022-01-21 14:01:38 -0500

    Add skeletons for VRF

---
+++ src/test/VRF.sol
@@ -0,0 +1,19 @@
+// // SPDX-License-Identifier: AGPL-3.0-only
+// pragma solidity 0.8.11;
+
+// import {DSTestPlus} from "./utils/DSTestPlus.sol";
+
+// import {VRF} from "../VRF.sol";
+
+// contract VRFTest is DSTestPlus {
+//     VRF vrf;
+
+//     function setUp() public {
+//         vrf = new VRF("rng");
+//     }
+
+//     function testSetGm() public {
+//         greeter.setGm("gm gm");
+//         greeter.gm("gm gm");
+//     }
+// }
